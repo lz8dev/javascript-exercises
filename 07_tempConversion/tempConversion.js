b51b325242f04f33deef2dea354e6a51b05dca70
lz8dev <kv.ramosh@gmail.com>
2023-06-07 08:52:46 -0500

    update

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5/9;
+  let res = tempC.toFixed(1);
+  return Number(res);    
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = tempC * 9/5 + 32;
+  let res = tempF.toFixed(1);
+  return Number(res);
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(x, y) {
+  let res = Number(x + y);
+	return res;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	let res = Number(x - y);
+	return res;
 };
 
-const sum = function() {
-	
+const sum = function(x, y, ...z) {
+	let res = 0;
+  for (num of arguments) {
+    res += num;
+  };
+  return Number(res);
 };
 
-const multiply = function() {
-
+const multiply = function(x, y, ...z) {
+	let res = 1;
+  for (num of arguments) {
+    res = res * num;
+  };
+  return Number(res);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  if (y === 0) {
+    res = 1;
+    return res;s
+  } else{
+    let res = x;
+    for (let i = 1; i < y; i++) {
+      res = x * res;
+    };
+    return res;
+  };
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return undefined;
+  } else if (num === 0) {
+      return 1;
+  } else {
+    for (let i = 0; i < num; i++) {
+      num *= i;
+    };
+  return num;
+  };
 };
 
 // Do not edit below this line
