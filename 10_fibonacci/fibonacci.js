cabca1a744b1e9feda6368a69ae51ee53a476086
lz8dev <kv.ramosh@gmail.com>
2023-06-07 22:47:18 -0500

    update solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [1, 1];
+    if (num < 0) { 
+        return 'OOPS';
+    };
+    if (num === 0 || num === 1) {
+        return 1;
+    };
+    for (let i = 2; i < num; i++) {
+        arr [i] = arr[i -1] + arr[i - 2];
+    };
+    return arr[num - 1];
 };
 
 // Do not edit below this line
